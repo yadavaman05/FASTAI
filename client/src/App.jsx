eaf6bfd0cf4a709729bd2bf945233e02a289198f
yadavaman05 <yadavamanmau@gmail.com>
2025-09-18 14:26:20 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import WriteArticle from './pages/WriteArticle'
+import BlogTitles from './pages/BlogTitles'
+
+import GenerateImages from './pages/GenerateImages'
+import RemoveBackground from './pages/RemoveBackground'
+import ReviewResume from './pages/ReviewResume'
+import RemoveObject from './pages/RemoveObject'
+import Community from './pages/Community'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/ai' element={<Layout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path='write-article' element={<WriteArticle/>}/>
+          <Route path='blog-titles' element={<BlogTitles/>}/>
+          <Route path='generate-images' element={<GenerateImages/>}/>
+          <Route path='remove-background' element={<RemoveBackground/>}/>
+          <Route path='remove-object' element={<RemoveObject/>}/>
+          <Route path='remove-resume' element={<ReviewResume/>}/>
+          <Route path='community' element={<Community/>}/>
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
